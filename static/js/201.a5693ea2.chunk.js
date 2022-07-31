07ebf96ef7b45bd924bf295fe441c8ea1b14d93e
Svetlana493 <93873804+Svetlana493@users.noreply.github.com>
2022-07-31 08:44:30 +0000

    Deploying to gh-pages from @ Svetlana493/goit-react-hw-05-movies@56de4127c25f11afaa554ff47a9b3cdd8c709ab9 🚀

---
+++ static/js/201.a5693ea2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[201],{8201:function(e,n,t){t.r(n),t.d(n,{default:function(){return r}});t(2791);var u=t(184);function r(){return(0,u.jsx)("h1",{children:"Page not found 404 "})}}}]);
+//# sourceMappingURL=201.a5693ea2.chunk.js.map
